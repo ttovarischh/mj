9faff54e0d5f3b9482618a3da733bebd17b6e7c6
ttovarischh <polinasot@gmail.com>
2021-12-15 20:42:35 +0300

    almost

--- dist/main.js
+++ dist/main.js
@@ -41,7 +41,7 @@ $(".headercont").click(function() {
   $(".cross").toggleClass("none");
 });
 
-$(".map, .card").hover(function(){
+$(".map, .card, a, .qa").hover(function(){
   $(".cursor").toggleClass("hoverc");
   $(".cursor").toggleClass("normal");
 });
@@ -105,34 +105,134 @@ $( ".type" ).click(function() {
 
 
 $(document).mousemove(function(e) {
-  let body_size_x = $( window ).width();
+  let body_size_x = $(".final_container").width();
   let half_body_size_x = parseInt(body_size_x / 2);
   let q_body_size_x = parseInt(half_body_size_x / 2);
+  let mine = parseInt(q_body_size_x / 2);
+  let kill = parseInt(mine / 2);
+
   let left_dir = e.pageX;
   let top_dir = e.pageY;
 
+  let eq1 = left_dir < kill;
+  let eq2 = left_dir < parseInt (kill * 4) ;
+  let eq3 = left_dir < parseInt (kill * 7) ;
+  let eq4 = left_dir < parseInt (kill * 8) ;
+  let eq5 = left_dir < parseInt (kill * 9) ;
+  let eq6 = left_dir < parseInt (kill * 10) ;
+  let eq7 = left_dir > parseInt (kill * 10) ;
+  let eq8 = left_dir < parseInt (kill * 11) ;
+  let eq9 = left_dir > parseInt (q_body_size_x * 3) ;
+  let e10 = left_dir > parseInt (q_body_size_x * 3.5) ;
 
-  let eq1 = left_dir < q_body_size_x;
-  let eq2 = left_dir < parseInt (q_body_size_x * 2) ;
-  let eq3 = left_dir < parseInt (q_body_size_x * 3) ;
-  let eq4 = left_dir < parseInt (q_body_size_x * 4) ;
+
+
+
+  let dead2 = left_dir < half_body_size_x;
+  if (dead2) {
+    $(".square2").css('display', 'none');
+ } else {
+   $(".square2").css('display', 'block');
+
+   if (eq9) {
+     console.log("eq9");
+     $(".square2").css("background-image", "url('./images/b3.jpg')");
+     $("body").css("background", "red");
+
+
+     $(".square2").css({
+     'top': '6%',
+     });
+  }
+ }
+
+  if (eq1) {
+    $(".square").css('display', 'none');
+ } else {
+   $(".square").css('display', 'block');
+ }
 
  if ($('.final_container').is(":hover")) {
+   $(".square").css({
+   left:e.clientX - 100
+   });
+   $(".square2").css({
+   left:e.clientX - 100
+   });
+   $(".square2").css('display', 'none');
+
+
+   $(".square").fadeIn();
    if (eq1) {
-    $("body").css("background-color","blue");
+    console.log("eq1");
   } else if (eq2) {
-   $("body").css("background-color","white");
+    $(".square2").css('display', 'none');
+
+    console.log("eq2");
+   $(".square").css("background-image", "url('./images/1.jpg')");
+   $(".square").css({
+   'top': '62%',
+   left:e.clientX - 100
+   });
   } else if (eq3) {
+    console.log("eq3");
+    $(".square").css("background-image", "url('./images/3.jpg')");
+    $(".square2").css('display', 'none');
 
-    $("body").css("background-color","red");
-    // $(".texture").removeClass("none");
+    $(".square").css({
+    'top': '9%',
+    left:e.clientX - 280
+    });
   } else if (eq4) {
-    $("body").css("background-color","green");
-  }
+    console.log("eq4");
+    $(".square").css({
+    'top': '30%',
+    left:e.clientX - 250
+    });
+    $(".square").css("background-image", "url('./images/4.jpg')");
+    $(".square2").css('display', 'none');
 
+  }
+  else if (eq5) {
+    console.log("eq5");
+    $(".square").css("background-image", "url('./images/5.jpg')");
+    $(".square").css('display', 'none');
+    $(".texture").fadeIn();
+    $(".square2").css('display', 'none');
+
+
+  } else if (eq6) {
+    console.log("eq6");
+    $(".square").hide();
+    $(".square2").css('display', 'none');
+
+
+  }
+  else if (eq7) {
+    $(".square").css('display', 'none');
+    $(".square2").css('display', 'block');
+    $(".square2").css("background-image", "url('./images/b1.jpg')");
+    $(".square2").css({
+    'top': '47%',
+    });
+  }
+  else if (eq8) {
+    $(".square2").css('display', 'block');
+    $(".square").css('display', 'none');
+    $(".square2").css("background-image", "url('./images/b1.jpg')");
+    $(".square2").css({
+    'top': '47%',
+    });
+  }
  } else {
-   $("body").css("background-color","black");
+  $(".square").css('display', 'none');
+  $(".square2").css('display', 'none');
+
+  // $(".texture").fadeOut();
 }
+
+
+
 
 $(".normal").css({
 left:e.clientX - 4,
